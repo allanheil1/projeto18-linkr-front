e40347958a65398ec4b05e0009e40ca37a3746a2
LPandorf <leticiapandorf@gmail.com>
2023-03-10 09:13:56 -0300

    feat: like, delete and edit icons

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai';
+import {TbTrashFilled} from 'react-icons/tb';
+import {TiPencil} from 'react-icons/ti';
 import * as S from './styles';
 
 function Posts(props) {
   const { id, name, photo, content, url, urlTitle, urlDescription, urlImage, setSearchResults, SetSearchQuery } = props;
   const navigate = useNavigate();
+  const [isLiked,setIsLiked]=useState(false);
 
   const redirectPage= (id)=>{
     navigate(`/user/${id}`)
     setSearchResults([])
     SetSearchQuery(name)
-}
+  }
+
+  function like(){
+    setIsLiked(!isLiked);
+  }
+
   return (
     <S.Container onChange={()=>redirectPage(id)}>
       <S.Content>
         <S.ProfilePic>
           <img src={photo} alt="" />
-          like
+          {isLiked ? <AiFillHeart onClick={like}/> : <AiOutlineHeart onClick={like}/>}
+          <S.Like>10 likes</S.Like>
         </S.ProfilePic>
         <S.PostContent>
-          <h3>{name}</h3>
+          <S.PostHeader>
+            <h3>{name}</h3>
+            <S.BySide>
+              <TiPencil/>
+              <TbTrashFilled/>
+            </S.BySide>
+          </S.PostHeader>
           <p>{content}</p>
           <S.Metadata onClick={() => window.open(url, '_blank', 'noopener,noreferrer')}>
             <div>
