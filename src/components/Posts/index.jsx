9c0138e58b796d7145564338aa50a9aa6fa1142a
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-03-23 10:48:30 -0300

    add layout comment

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { AiOutlineHeart, AiFillHeart, AiOutlineComment } from 'react-icons/ai';
+import { GoGitCompare } from 'react-icons/go';
+import { BsSend } from 'react-icons/bs';
 import { TbTrashFilled } from 'react-icons/tb';
 import { TiPencil } from 'react-icons/ti';
 import { ReactTagify } from 'react-tagify';
@@ -8,6 +10,7 @@ import { getLikes, giveALike, takeALike } from '../../service';
 import * as S from './styles';
 
 function Posts(props) {
+  const [open, setOpen] = useState(false)
   const {
     id,
     postId,
@@ -82,36 +85,76 @@ function Posts(props) {
   // }
 
   return (
-    <S.Container data-test="post">
-      <S.Content>
-        <S.ProfilePic>
-          <img src={photo} alt="" />
-          {/* {isLiked ? <AiFillHeart onClick={like} /> : <AiOutlineHeart onClick={like} />}
+    <>
+      <S.Container data-test="post">
+        <S.Content>
+          <S.ProfilePic>
+            <img src={photo} alt="" />
+            {/* {isLiked ? <AiFillHeart onClick={like} /> : <AiOutlineHeart onClick={like} />}
           <S.Like>{qLikes} likes</S.Like> */}
-        </S.ProfilePic>
-        <S.PostContent>
-          <S.PostHeader>
-            <h3 data-test="username" onClick={() => redirectPage(id)}>
-              {name}
-            </h3>
-            <S.BySide>
-              <TiPencil />
-              <TbTrashFilled />
-            </S.BySide>
-          </S.PostHeader>
-          <ReactTagify tagStyle={S.tagStyle} tagClicked={(tag) => navigate(`/hashtag/${tag.replace("#","")}`)}>
-            <p data-test="description">{content}</p>
-          </ReactTagify>
-          <S.Metadata data-test="link" href={url} target="_blank" rel="noopener noreferrer">
-            <div>
-              <h3>{urlTitle}</h3>
-              <p> {urlDescription}</p>
+            <div onClick={()=> setOpen(!open)}>
+              <AiOutlineComment fontSize={23} />
+              209
             </div>
-            <img src={urlImage} alt="" />
-          </S.Metadata>
-        </S.PostContent>
-      </S.Content>
-    </S.Container>
+            <div>
+              <GoGitCompare fontSize={21} />
+              176
+            </div>
+          </S.ProfilePic>
+          <S.PostContent>
+            <S.PostHeader>
+              <h3 data-test="username" onClick={() => redirectPage(id)}>
+                {name}
+              </h3>
+              <S.BySide>
+                <TiPencil />
+                <TbTrashFilled />
+              </S.BySide>
+            </S.PostHeader>
+            <ReactTagify tagStyle={S.tagStyle} tagClicked={(tag) => navigate(`/hashtag/${tag.replace('#', '')}`)}>
+              <p data-test="description">{content}</p>
+            </ReactTagify>
+            <S.Metadata data-test="link" href={url} target="_blank" rel="noopener noreferrer">
+              <div>
+                <h3>{urlTitle}</h3>
+                <p> {urlDescription}</p>
+              </div>
+              <img src={urlImage} alt="" />
+            </S.Metadata>
+          </S.PostContent>
+        </S.Content>
+      </S.Container>
+      {open?<S.Comment>
+        <ul>
+          <li>
+            <S.UserImg>
+              <img src="aquiMinhaImagem" alt="Eu" />
+            </S.UserImg>
+            <div>
+              <S.UserConteiner>
+                <p>Name</p>
+                <div>
+                  <spam></spam>following
+                </div>
+              </S.UserConteiner>
+              <div>Aqui está meu cooomentario</div>
+            </div>
+          </li> 
+        </ul>
+        <S.Input>
+          <S.UserImg>
+            <img src="imagem" alt="eu" />
+          </S.UserImg>
+          <S.InputComment>
+            <input type="text" placeholder="write a comment..." />
+            <p>
+              <BsSend fontSize={20} />
+            </p>
+          </S.InputComment>
+        </S.Input>
+      </S.Comment> : "" }
+      
+    </>
   );
 }
 
--- src/pages/AuthPages/SignInPage.js
+++ src/pages/AuthPages/SignInPage.js
@@ -1,8 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-
-import { UserContext } from "../../contexts/UserContext";
 import * as S from './style';
 
 export default function SignInPage(){
